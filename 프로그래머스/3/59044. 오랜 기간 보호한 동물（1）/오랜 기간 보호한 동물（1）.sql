04a3cf5f503b8fffd7381e472ad0674292d60346
KKdohyeong <106377690+KKdohyeong@users.noreply.github.com>
2025-11-14 21:25:00 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,12 @@
-SELECT 
-    AI.NAME,
-    AI.DATETIME
-FROM ANIMAL_INS AS AI
-LEFT JOIN ANIMAL_OUTS AS AO
-       ON AI.ANIMAL_ID = AO.ANIMAL_ID
-WHERE AO.ANIMAL_ID IS NULL                -- 입양(ANIMAL_OUTS)에 기록이 없는 동물만
-ORDER BY AI.DATETIME ASC                  -- 보호 시작일이 가장 오래된 순(오름차순)
-LIMIT 3;                                  -- 그중 상위 3마리
+-- 코드를 입력하세요
+with ANIMAL_STIL_IN as (
+    SELECT AI.NAME, AI.DATETIME
+    from ANIMAL_INS AI left join ANIMAL_OUTS AO
+    on AI.ANIMAL_ID = AO.ANIMAL_ID
+    where AO.DATETIME IS NULL
+    order by AI.DATETIME ASC
+) 
+
+SELECT *
+from ANIMAL_STIL_IN
+where ROWNUM <=3
