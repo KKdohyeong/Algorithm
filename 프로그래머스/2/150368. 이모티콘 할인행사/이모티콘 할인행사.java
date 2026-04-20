322a6b62556f57b77b33fb9cc5582bd97655cbbb
KKdohyeong <106377690+KKdohyeong@users.noreply.github.com>
2026-04-20 10:30:37 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 90.82 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,70 +1,87 @@
 import java.util.*;
+import java.io.*;
+/*
+1. 이모티콘 플러스 가입자를 최대한 늘려야함
+
+2. 이모티콘 판매액을 최대한 늘려야해.
+
+n은 100명까지, m은 7개까지 (4개의 할인율 가능)
+
+
+*/
 
 class Solution {
     
-    // 가능한 할인율 배열
-    static int[] DISCOUNT_RATES = {10, 20, 30, 40};
-    // 각 이모티콘에 적용된 할인율을 저장할 배열
-    static int[] currentDiscounts;
-    // 최종 결과를 저장할 변수
-    static int maxSubscribers = 0;
-    static int maxSales = 0;
+    public int[] discounts = {10, 20, 30, 40};
+    public int max_people = 0;
+    public int max_money = 0;
     
-    public int[] solution(int[][] users, int[] emoticons) {
-        currentDiscounts = new int[emoticons.length];
+    public void traverse(int depth, int[][] users, int[] emoticons, int[] emoticon_discounts){
         
-        // DFS 시작 (0번째 이모티콘부터 할인율 결정 시작)
-        dfs(0, users, emoticons);
-        
-        int[] answer = {maxSubscribers, maxSales};
-        return answer;
-    }
-    
-    /**
-     * @param depth 현재 할인율을 결정할 이모티콘의 인덱스
-     */
-    public void dfs(int depth, int[][] users, int[] emoticons) {
-        // [종료 조건] 모든 이모티콘의 할인율이 결정되었을 때
-        if (depth == emoticons.length) {
-            int currentSubscribers = 0;
-            int currentSales = 0;
+        //계산시작
+        if(depth == emoticons.length){
+            int[] user_buy = new int[users.length];
+            int here_people = 0;
+            int here_total_money = 0;
             
-            // 모든 유저에 대해 구매 비용 계산
-            for (int[] user : users) {
-                int userMinDiscount = user[0];
-                int userMaxCost = user[1];
-                int totalCost = 0;
+            Arrays.fill(user_buy, 0);
+            
+            //이제 돌면서 다 더하자.
+            for(int i=0; i<emoticons.length; i++){
                 
-                // 각 이모티콘을 살지 말지 결정
-                for (int i = 0; i < emoticons.length; i++) {
-                    if (currentDiscounts[i] >= userMinDiscount) {
-                        totalCost += emoticons[i] * (100 - currentDiscounts[i]) / 100;
+                for(int j=0; j<users.length; j++){
+                    if(emoticon_discounts[i] >= users[j][0]){
+                        user_buy[j] += (emoticons[i] / 100) * (100 - emoticon_discounts[i]) ;
                     }
                 }
-                
-                // 유저의 기준에 따라 플러스 가입 또는 구매 확정
-                if (totalCost >= userMaxCost) {
-                    currentSubscribers++;
-                } else {
-                    currentSales += totalCost;
+            }
+            
+            //이제 최종계산하자
+            for(int i=0; i<users.length; i++){
+                //System.out.printf("%d번쨰 사람은 %d가격을 샀어\n", i, user_buy[i]);
+                if(user_buy[i] >= users[i][1]){
+                    here_people++;
+                }
+                else{
+                    here_total_money+=user_buy[i];
                 }
             }
             
-            // 최종 결과 업데이트
-            if (currentSubscribers > maxSubscribers) {
-                maxSubscribers = currentSubscribers;
-                maxSales = currentSales;
-            } else if (currentSubscribers == maxSubscribers) {
-                maxSales = Math.max(maxSales, currentSales);
+            if(here_people > max_people){
+                max_people = here_people;
+                max_money = here_total_money;
+            }
+            else if(here_people == max_people){
+                max_money = Math.max(max_money, here_total_money);
             }
             
-            return; // 탐색 종료
+            
+            return;
+            
         }
         
-        // [재귀] 현재 이모티콘(depth)의 할인율을 바꿔가며 다음 단계로 진행
-        for (int rate : DISCOUNT_RATES) {
-            currentDiscounts[depth] = rate;
-            dfs(depth + 1, users, emoticons);
+        for(int i=0; i<4; i++){
+            emoticon_discounts[depth] = discounts[i];
+            
+            traverse(depth+1, users, emoticons, emoticon_discounts);   
         }
+        
+        
+    }
+    
+    public int[] solution(int[][] users, int[] emoticons) {
+        int[] emoticon_discounts = new int[emoticons.length];
+        
+        
+        
+        traverse(0, users, emoticons, emoticon_discounts);
+        
+        
+        int[] answer = new int[2];
+        answer[0] = max_people;
+        answer[1] = max_money;
+        
+        
+        return answer;
     }
 }
