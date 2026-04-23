19fc7686d71d5484567d925ee9470f16f59c744c
KKdohyeong <106377690+KKdohyeong@users.noreply.github.com>
2026-04-23 15:09:15 +0900

    [level 2] Title: 타겟 넘버, Time: 28.12 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,55 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+/*
+숫자 갯수는 20개네. 완전탐색? 모든경우의수 해봐야함.
+
+*/
+
 class Solution {
     
-    int calculatePlus(int[] numbers, int target, int depth, int sum){
-        int result = 0;
-        if(depth<=numbers.length){
-            sum += numbers[depth-1];
-        }
-        
-        if(depth==numbers.length){
-            //System.out.printf("합계는 %d\n", sum);
-            if(sum==target){
-                System.out.println("다 돼");
-                return 1;
-            }
-            if(sum!=target){
-                return 0;
-            }
-        }
-        
-        result += calculatePlus(numbers, target, depth+1, sum);
-        result += calculateMinus(numbers, target, depth+1, sum);
-        
-        return result;
-    }
+    public int answer = 0;
     
-    int calculateMinus(int[] numbers, int target, int depth, int sum){
-        int result = 0;
-        if(depth<=numbers.length){
-            sum -= numbers[depth-1];
+    public void traverse(int depth, int[] numbers, int target){
+        //계산하기
+        if(depth == numbers.length){
+            int total = 0;
+            for(int i=0; i<numbers.length; i++){
+                total+=numbers[i];
+            }
+            
+            if(total == target){
+                answer++;
+            }
+            return;
         }
         
-        if(depth==numbers.length){
-            if(sum==target){
-                return 1;
-            }
-            if(sum!=target){
-                return 0;
-            }
-        }
+        //+로 넘기기
+        traverse(depth+1, numbers, target);
+        //-로 넘기기, 그리고 돌려놓을필요 없어보이기도 하는데
+        numbers[depth] = -numbers[depth];
+        traverse(depth+1, numbers, target);
+        numbers[depth] = -numbers[depth];
         
-        result += calculatePlus(numbers, target, depth+1, sum);
-        result += calculateMinus(numbers, target, depth+1, sum);
-        //System.out.printf("result is %d and sum is %d\n", result, sum);
-        return result;
+        
+        
+        
     }
     
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        int depth = 1;
-        answer += calculatePlus(numbers, target, 1, 0);
-        answer += calculateMinus(numbers, target, 1, 0);
+        
+        traverse(0, numbers, target);
         return answer;
     }
-    
 }
