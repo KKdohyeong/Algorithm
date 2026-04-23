a7ab496a159a62605070805628029b5191958281
KKdohyeong <106377690+KKdohyeong@users.noreply.github.com>
2026-04-23 13:12:43 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 0.04 ms
+메모리: 74.1 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 07일 23:26:15
+2026년 04월 23일 13:12:34
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,35 +1,40 @@
 import java.util.*;
+import java.io.*;
+    
+
+/*
+걍 다 해보면 될거같은데?
+
+세로 1
+세로 2
+
+*/
+
 class Solution {
-    public static int[] solution(int brown, int yellow) {
+    public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        int map[][];
-        int total = brown+yellow;
-        
-        // 카펫의 길이는 가로가 더 길거나 같다
-        for(int i=1; i<=total; i++) {
-            if(total%i==0) {
-                int row = i;
-                int col = total/row;
-                
-                if(row > col) {
-                    continue;
-                }
-                
-                int inside_block = (row-2)*(col-2);
-                if(inside_block == yellow) {
-                    answer[0] = col;
-                    answer[1] = row;
-                    return answer;
-                }
+        for(int row=1; row<= yellow; row++){
+            if(yellow % row != 0){
+                continue;
             }
+            int col = yellow / row;
+
+            
+            int total_need = 4 + 2*row + 2*col;
+            
+            //System.out.printf("(%d,%d)로 %d개\n", row, col, total_need);
+            
+            
+            //0에는 col 1에는 row
+            if(total_need == brown){
+                answer[0] = col+2;
+                answer[1] = row+2;
+                break;
+            }
+            
+            
         }
-        
-        
-        
-        
-        
-        
         
         return answer;
     }
